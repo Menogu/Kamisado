172c8b68d542c097c15b08686bdc805cc823c2bf
Menogu <boba11@partyheld.de>
2017-07-31 15:11:40 +0200

    added hello again 2

--- ITProject/src/philipp/Main.java
+++ ITProject/src/philipp/Main.java
@@ -5,8 +5,8 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.print("Hello");
-        System.out.print("aganin Hello");
+        System.out.print("again Hello");
 
-        System.out.print("aganin Hello");
+        System.out.print("Hello from the other sideee");
     }
 }
